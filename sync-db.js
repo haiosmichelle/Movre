076de17decf1a291066f46c283f047b4324d8141
haiosmichelle <94860504+haiosmichelle@users.noreply.github.com>
2024-06-09 17:04:01 +0300

    remake

--- sync-db.js
+++ sync-db.js
@@ -1,14 +1,6 @@
 const { sequelizePostgres } = require('./dataBase/database');
-const fs = require('fs');
-const path = require('path');
+const models = require('./models');
 
-// Citește toate fișierele din directorul models și le importă
-const modelsDir = path.join(__dirname, 'models');
-fs.readdirSync(modelsDir).forEach(file => {
-  if (file.endsWith('.js')) {
-    require(path.join(modelsDir, file));
-  }
-});
 // Sincronizează toate modelele
 sequelizePostgres.sync({ force: true })
   .then(() => {
