435b7dc57adb7a91f8a09b8d1550f944085e2cbb
haiosmichelle <94860504+haiosmichelle@users.noreply.github.com>
2024-06-09 17:29:59 +0300

    add population on db

--- models/index.js
+++ models/index.js
@@ -1,63 +1,27 @@
-const { sequelizePostgres } = require('./dataBase/database');
-const models = require('./models');
+const fs = require('fs');
+const path = require('path');
+const { sequelizePostgres } = require('../dataBase/database');
 
-// Sincronizează toate modelele
-sequelizePostgres.sync({ force: true })
-  .then(async () => {
-    console.log('Tabelele au fost create cu succes.');
+const models = {};
 
-    // Adaugă date inițiale în tabele
+// Citește toate fișierele din directorul models și le importă
+const modelsDir = path.join(__dirname);
+fs.readdirSync(modelsDir).forEach(file => {
+  if (file !== 'index.js' && file.endsWith('.js')) {
+    const model = require(path.join(modelsDir, file));
+    models[model.name] = model;
+  }
+});
 
-    await models.Admin.create({
-      email: 'admin@gmail.com',
-      password: '1234'
-    });
+// Definirea relațiilor între modele (dacă există)
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
 
-    await models.User.create({
-      name: 'John Doe',
-      email: 'john@example.com',
-      password: '12345678',
-      birth_date: new Date(1990, 1, 1)
-    });
-
-    await models.Movie.create({
-      name: 'Inception',
-      type: 'Sci-Fi',
-      runtime: 148,
-      release_year: 2010,
-      picture: 'Lady-Bird.jpg',
-      raiting: 0,
-      description: 'A thief who steals corporate secrets through the use of dream-sharing technology.'
-    });
-
-    await models.Rating.create({
-      movieId: 1,
-      userId: 1,
-      star: 5
-    });
-
-    await models.Review.create({
-      movieId: 1,
-      userId: 1,
-      message: 'Great movie!',
-      post_date: new Date(),
-      like: 10
-    });
-
-    await models.LikeReview.create({
-      ReviewId: 1,
-      userId: 1
-    });
-
-    await models.WatchList.create({
-      MovieId: 1,
-      userId: 1
-    });
-
-    console.log('Datele inițiale au fost adăugate cu succes.');
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error('Eroare la crearea tabelelor: ', err);
-    process.exit(1);
-  });
+module.exports = {
+  sequelize: sequelizePostgres,
+  Sequelize: require('sequelize'),
+  ...models
+};
